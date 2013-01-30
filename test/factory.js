c77e7e0208f5e599af81dc2593467c60560e3150
Zach Smith <zach@amicushq.com>
2013-01-30 13:06:04 -0500

    w00t random strings in fields

--- test/factory.js
+++ test/factory.js
@@ -20,4 +20,13 @@ describe("When I specify a factory", function() {
     expect(model1.get("title")).to.be("Title Number 1")
     expect(model2.get("title")).to.be("Title Number 2")
   }) 
+  it("It should replace random strings", function() {
+    var modelFactory = new Factory(Backbone.Model, {
+      title: "{{random(24, 0-9a-f)}}" //title that starts with
+    })
+    var model1 = modelFactory.generate()
+    var model2 = modelFactory.generate()
+    expect(model1.get("title").length).to.be(24)
+    expect(model2.get("title")).to.match(/[a-z0-9]{24}/)
+  })
 })
