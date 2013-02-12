7e6a39747b1f996dc7ff6b2d0b3a42258fb60a69
Zach Smith <zach@amicushq.com>
2013-02-11 23:58:59 -0500

    Factory for collections, w00t

--- test/factory.js
+++ test/factory.js
@@ -1,6 +1,7 @@
 var Factory = require("../lib/factory")
   , Backbone = require("backbone")
   , expect = require("expect.js")
+  , CollectionFactory = Factory.Collection
 
 describe("When I specify a factory", function() {
 
@@ -8,15 +9,15 @@ describe("When I specify a factory", function() {
     var modelFactory = new Factory(Backbone.Model, {
       title: "This is a title"
     })
-    var model = modelFactory()
+    var model = modelFactory.generate()
     expect(model.get("title")).to.be("This is a title")
   })
   it("It should replace sequences", function() {
     var modelFactory = new Factory(Backbone.Model, {
       title: "Title Number {{sequence(1)}}" //title that starts with
     })
-    var model1 = modelFactory()
-    var model2 = modelFactory()
+    var model1 = modelFactory.generate()
+    var model2 = modelFactory.generate()
     expect(model1.get("title")).to.be("Title Number 1")
     expect(model2.get("title")).to.be("Title Number 2")
   }) 
@@ -24,8 +25,8 @@ describe("When I specify a factory", function() {
     var modelFactory = new Factory(Backbone.Model, {
       title: "{{random(24, 0-9a-f)}}" //title that starts with
     })
-    var model1 = modelFactory()
-    var model2 = modelFactory()
+    var model1 = modelFactory.generate()
+    var model2 = modelFactory.generate()
     expect(model1.get("title").length).to.be(24)
     expect(model2.get("title")).to.match(/[a-z0-9]{24}/)
   })
@@ -35,7 +36,18 @@ describe("When I specify a factory", function() {
         return "I'm a title yo"       
       }//title that starts with
     })
-    var model = modelFactory()
+    var model = modelFactory.generate()
     expect(model.get("title")).to.be("I'm a title yo")
+  })
+  it("It should replace sequences", function() {
+    var modelFactory = new Factory(Backbone.Model, {
+      title: "Title Number {{sequence(1)}}" //title that starts with
+    })
+    var models = modelFactory.collection(Backbone.Collection, 4)
+
+    expect(models.at(0).get("title")).to.be("Title Number 1")
+    expect(models.at(1).get("title")).to.be("Title Number 2")
+    expect(models.at(2).get("title")).to.be("Title Number 3")
+    expect(models.at(3).get("title")).to.be("Title Number 4")
   })
 })
